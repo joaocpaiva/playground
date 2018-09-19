50d951fc742e9214f9e3404c3f42e22d71a80ed2
Joao Paiva <jpaiva@microsoft.com>
2018-09-18 17:35:20 -0700

    [Tree] Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).

---
+++ Algorithms/Algorithms.Managed/Tree/LevelOrder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    /// <summary>
+    /// Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+    /// 
+    /// For example: given binary tree[3, 9, 20, null, null, 15, 7],
+    ///    3
+    ///   / \
+    ///  9  20
+    ///    /  \
+    ///   15   7
+    ///
+    /// return its level order traversal as:
+    /// [
+    ///   [3],
+    ///   [9,20],
+    ///   [15,7]
+    /// ]
+    /// </summary>
+    public class LevelOrder
+    {
+        public IList<IList<int>> Compute(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            Stack<Tuple<TreeNode, int>> stack = new Stack<Tuple<TreeNode, int>>();
+
+            stack.Push(new Tuple<TreeNode, int>(root, 0));
+            while (stack.Any())
+            {
+                var tuple = stack.Pop();
+                var node = tuple.Item1;
+                var level = tuple.Item2;
+
+                if (node != null)
+                {
+                    if (result.Count == level)
+                    {
+                        result.Add(new List<int>());
+                    }
+
+                    result[level].Add(node.val);
+
+                    stack.Push(new Tuple<TreeNode, int>(node.right, level + 1));
+                    stack.Push(new Tuple<TreeNode, int>(node.left, level + 1));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms/AlgorithmsTests/Tree/LevelOrderTests.cs
@@ -0,0 +1,38 @@
+﻿using Algorithms.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTests.Tree
+{
+    [TestClass]
+    public class LevelOrderTests
+    {
+        [TestMethod]
+        public void TestLevelOrder()
+        {
+            var levelOrder = new LevelOrder();
+
+            TreeNode tree = new TreeNode(3);
+            tree.left = new TreeNode(9);
+            tree.right = new TreeNode(20);
+
+            tree.right.left = new TreeNode(15);
+            tree.right.right = new TreeNode(7);
+
+            var result = levelOrder.Compute(tree);
+            var expected = new List<IList<int>>
+            {
+                new List<int> { 3 },
+                new List<int> { 9, 20 },
+                new List<int> { 15, 7 }
+            };
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                CollectionAssert.AreEqual(expected[i].ToArray(), result[i].ToArray());
+            }
+        }
+    }
+}
